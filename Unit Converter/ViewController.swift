34152c06b433d99e5c7d3b672c286c65761c2f3d
Bo Ning Li <leoli212@hotmail.com>
2019-01-21 15:57:57 -0800

    Initial Commit

---
+++ Unit Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unit Converter
+//
+//  Created by Bo Ning Li on 1/21/19.
+//  Copyright © 2019 Langara College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
